ea1c32abf9ff8cab3ab7064e9bca85628cfe3ff4
Blade Barringer <blade@crookedneighbor.com>
2016-04-25 08:11:20 -0500

    feat: Add emoji support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var markdownit = require('markdown-it');
 var linkifyImagesPlugin = require('markdown-it-linkify-images');
 var linkTargetPlugin = require('markdown-it-link-target');
+var emojiPlugin = require('./plugins/emoji');
 
 var md = markdownit({
   linkify: true,
@@ -12,6 +13,7 @@ var md = markdownit({
   target: '_blank',
   linkClass: 'markdown-img-link',
   imgClass: 'markdown-img',
-});
+})
+.use(emojiPlugin);
 
 module.exports = md;
--- test/index.js
+++ test/index.js
@@ -15,4 +15,10 @@ describe('Habitica Markdown', function () {
 
     expect(result).to.contain('<a href="https://example.com/image.png" class="markdown-img-link" target="_blank"><img src="https://example.com/image.png" alt="image" class="markdown-img"></a>');
   });
+
+  it('renders emoji from s3', function () {
+    let result = md.render('foo :smile: bar');
+
+    expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/smile.png" alt="smile"> bar');
+  });
 });
