198640961aeb85abd5ed614c7e6597fd9ab3a52d
Blade Barringer <blade@crookedneighbor.com>
2016-04-26 20:03:06 -0500

    fix: Fail when dist is not committed

--- dist/habitica-markdown.js
+++ dist/habitica-markdown.js
@@ -8815,12 +8815,12 @@ module.exports.Z   = require('./categories/Z/regex');
 },{"./categories/Cc/regex":71,"./categories/Cf/regex":72,"./categories/P/regex":73,"./categories/Z/regex":74,"./properties/Any/regex":76}],76:[function(require,module,exports){
 module.exports=/[\0-\uD7FF\uDC00-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF]/
 },{}],77:[function(require,module,exports){
-var emoji = require('markdown-it-emoji');
+var markdownitEmoji = require('markdown-it-emoji');
 
 // https://github.com/markdown-it/markdown-it-emoji/tree/4d5f6af1b6efb0975dae2ac51dbe6252636724aa#change-output
-function emojiPlugin (md, options) {
-  md.use(emoji);
-  md.renderer.rules.emoji = function(token, idx) {
+function emojiPlugin (md) {
+  md.use(markdownitEmoji);
+  md.renderer.rules.emoji = function markdownEmojiRules (token, idx) {
     var emoji = token[idx];
     var src = 'https://s3.amazonaws.com/habitica-assets/cdn/emoji/' + emoji.markup + '.png';
     var style = 'height: 1.5em; width: 1.5em';
