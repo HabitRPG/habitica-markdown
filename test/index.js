511afabd26ed499b6cb4a30a0cec30b8e34f91c1
Blade Barringer <blade@crookedneighbor.com>
2016-05-27 07:47:39 -0500

    test: pend shortcut test
    See https://github.com/HabitRPG/habitica-markdown-emoji/issues/4

--- test/index.js
+++ test/index.js
@@ -28,7 +28,7 @@ describe('Habitica Markdown', function () {
     expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/thumbsup.png" alt="thumbsup"> bar');
   });
 
-  it('renders shortcut emoji from s3', function () {
+  xit('renders shortcut emoji from s3', function () {
     let result = md.render('foo :) bar');
 
     expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/smiley.png" alt="smiley"> bar');
